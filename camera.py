219f31aec2f50d866a8aac53798c49c3401b1fcc
Sarah <87944187+007Sarah007@users.noreply.github.com>
2024-02-01 09:11:09 +0100

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,132 @@
+import pygame, sys
+from random import randint
+
+class Tree(pygame.sprite.Sprite):
+	def __init__(self,pos,group):
+		super().__init__(group)
+		self.image = pygame.image.load('graphics/tree.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self,pos,group):
+		super().__init__(group)
+		self.image = pygame.image.load('graphics/player.png').convert_alpha()
+		self.rect = self.image.get_rect(center = pos)
+		self.direction = pygame.math.Vector2()
+		self.speed = 20
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_z]:
+			self.direction.y = -1
+		elif keys[pygame.K_s]:
+			self.direction.y = 1
+		else:
+			self.direction.y = 0
+
+		if keys[pygame.K_d]:
+			self.direction.x = 1
+		elif keys[pygame.K_q]:
+			self.direction.x = -1
+		else:
+			self.direction.x = 0
+
+	def update(self):
+		self.input()
+		self.rect.center += self.direction * self.speed
+
+class CameraGroup(pygame.sprite.Group):
+	def __init__(self):
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+
+		# camera offset 
+		self.offset = pygame.math.Vector2()
+		self.half_w = self.display_surface.get_size()[0] // 2
+		self.half_h = self.display_surface.get_size()[1] // 2
+
+		# box setup
+		self.camera_borders = {'left': 200, 'right': 200, 'top': 100, 'bottom': 100}
+		l = self.camera_borders['left']
+		t = self.camera_borders['top']
+		w = self.display_surface.get_size()[0]  - (self.camera_borders['left'] + self.camera_borders['right'])
+		h = self.display_surface.get_size()[1]  - (self.camera_borders['top'] + self.camera_borders['bottom'])
+		self.camera_rect = pygame.Rect(l,t,w,h)
+
+		# ground
+		self.ground_surf = pygame.image.load('graphics/ground.png').convert_alpha()
+		self.ground_rect = self.ground_surf.get_rect(topleft = (0,0))
+
+		# camera speed
+		self.keyboard_speed = 10
+		self.mouse_speed = 1
+
+		# zoom 
+		self.zoom_scale = 1
+		self.internal_surf_size = (2500,2500)
+		self.internal_surf = pygame.Surface(self.internal_surf_size, pygame.SRCALPHA)
+		self.internal_rect = self.internal_surf.get_rect(center = (self.half_w,self.half_h))
+		self.internal_surface_size_vector = pygame.math.Vector2(self.internal_surf_size)
+		self.internal_offset = pygame.math.Vector2()
+		self.internal_offset.x = self.internal_surf_size[0] // 2 - self.half_w
+		self.internal_offset.y = self.internal_surf_size[1] // 2 - self.half_h
+
+	def center_target_camera(self,target):
+		self.offset.x = target.rect.centerx - self.half_w
+		self.offset.y = target.rect.centery - self.half_h
+
+	def custom_draw(self,player):
+		self.center_target_camera(player)
+
+		self.internal_surf.fill('#71ddee')
+
+		# ground 
+		ground_offset = self.ground_rect.topleft - self.offset + self.internal_offset
+		self.internal_surf.blit(self.ground_surf,ground_offset)
+
+		# active elements
+		for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset + self.internal_offset
+			self.internal_surf.blit(sprite.image,offset_pos)
+
+		scaled_surf = pygame.transform.scale(self.internal_surf,self.internal_surface_size_vector * self.zoom_scale)
+		scaled_rect = scaled_surf.get_rect(center = (self.half_w,self.half_h))
+
+		self.display_surface.blit(scaled_surf,scaled_rect)
+
+
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+pygame.event.set_grab(True)
+
+# setup 
+camera_group = CameraGroup()
+player = Player((640,360),camera_group)
+
+for i in range(20):
+	random_x = randint(1000,2000)
+	random_y = randint(1000,2000)
+	Tree((random_x,random_y),camera_group)
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				pygame.quit()
+				sys.exit()
+
+		if event.type == pygame.MOUSEWHEEL:
+			camera_group.zoom_scale += event.y * 0.03
+
+	screen.fill('#71ddee')
+
+	camera_group.update()
+	camera_group.custom_draw(player)
+
+	pygame.display.update()
+	clock.tick(60)
