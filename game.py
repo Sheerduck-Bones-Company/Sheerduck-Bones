6e61d8746febfed661bf82b4edb9139a8f24da0e
Gandalfph <150068459+Gandalfph@users.noreply.github.com>
2024-02-01 09:42:43 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,15 @@
+import pygame
+import player
+import mape
 
+class Game():
+    def __init__(self):
+        self.is_playing = False
+        self.map = mape.Map()
+        self.player = player.Player(self)
+
+    def start(self):
+        self.is_playing = True
+    
+    def stop(self):
+        self.is_playing = False
