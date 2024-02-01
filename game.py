9ce79b54125cfa01e874d6d61cfc29d68bf3d5a4
Gandalfph <150068459+Gandalfph@users.noreply.github.com>
2024-02-01 14:22:24 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,15 +1,15 @@
-import pygame
-import player
-import mape
+import pygame, player, mape, camera
 
 class Game():
     def __init__(self):
         self.is_playing = False
         self.map = mape.Map(self)
         self.player = player.Player(self)
+        self.camera_group = camera.CameraGroup()
+        self.player = player.Player((640,360),self.camera_group)
 
     def start(self):
         self.is_playing = True
-    
+
     def stop(self):
         self.is_playing = False
