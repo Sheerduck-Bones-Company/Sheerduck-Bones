03c7d80631f0211190c2b1a8d03013840ffad081
Sarah <87944187+007Sarah007@users.noreply.github.com>
2024-02-01 09:24:55 +0100

    Update and rename Ecran accueil to Boucle_principale.py

---
+++ Boucle_principale.py
@@ -0,0 +1,38 @@
+import pygame, sys
+from random import 
+from camera.py import CameraGroup, Player, Tree
+
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+clock = pygame.time.Clock()
+pygame.event.set_grab(True)
+
+# setup 
+camera_group = CameraGroup()
+player = Player((640,360),camera_group)
+
+for i in range(20):
+	random_x = randint(1000,2000)
+	random_y = randint(1000,2000)
+	Tree((random_x,random_y),camera_group)
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				pygame.quit()
+				sys.exit()
+
+		if event.type == pygame.MOUSEWHEEL:
+			camera_group.zoom_scale += event.y * 0.03
+
+	screen.fill('#71ddee')
+
+	camera_group.update()
+	camera_group.custom_draw(player)
+
+	pygame.display.update()
+	clock.tick(60)
