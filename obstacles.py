bf40940f110292c9e6d7a041e7e3fffc59526f83
Gandalfph <150068459+Gandalfph@users.noreply.github.com>
2024-02-01 09:55:45 +0100

    Create obstacles.py

---
+++ obstacles.py
@@ -0,0 +1,7 @@
+import pygame
+
+class Tree(pygame.sprite.Sprite):
+	def __init__(self,pos,group):
+		super().__init__(group)
+		self.image = pygame.image.load('graphics/tree.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
