bf143a0b23268e93054933e602b255038a00fb40
Vitessefraud <158447698+Vitessefraud@users.noreply.github.com>
2024-02-08 09:38:08 +0100

    Update animation.py
    - ajouter les animations pour les autres persos

--- animation.py
+++ animation.py
@@ -7,6 +7,11 @@ class AnimateCharacter(pygame.sprite.Sprite):
 #charge les images des personnages :
 def chargement_img_sprite(sprite_name):
   img = []
-  path = f"graphics/{sprite_name}/{sprite_name}"                                          #Le premier sprite name correspond au dossier et le deuxieme au nom du fichier
-  for nombre in range(a,b):                                                                #a est l'indice de la premiere image et b la derniere
+  path = f"graphics/{sprite_name}/{sprite_name}"                                           #Le premier sprite name correspond au dossier et le deuxieme au nom du fichier
+  for nombre in range(a,b):                                                                 #a est l'indice de la premiere image et b la derniere
     img_path = path + nombre +'.png'
+    pygame.image.load(img_path)
+    img.append(pygame.image.load(img_path))
+return img
+#creer dico qui contient toutes les animations des personnages
+dico{sheerduck : chargement_img_sprite('sheerduck')}                                        #modifier le dico pour les autres persos
