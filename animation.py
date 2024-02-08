acb464004bf911e2a0601d60f8ed0d264eb49156
Vitessefraud <158447698+Vitessefraud@users.noreply.github.com>
2024-02-08 09:15:13 +0100

    Update animation.py

--- animation.py
+++ animation.py
@@ -1,4 +1,10 @@
 import pygame
+class AnimateCharacter(pygame.sprite.Sprite):
+  def __init__(self, sprite_name):
+    super().__init__()
+    self.image = pygame.image.load('graphics/' + sprite_name + '.png')                    #à modifier si besoin
 
-def helloWorld(message):
-  print(message)
+#charge les images des personnages :
+def chargement_img_sprite(sprite_name):
+  img = []
+  path = f"graphics/{sprite_name}/{sprite_name}"                                          #Le premier sprite name correspond au dossier et le deuxieme au nom du fichier
